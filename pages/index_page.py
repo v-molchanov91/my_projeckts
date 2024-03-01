99770ff1d4de4576f06bce1c1ac36396b0f08f44
v.molchanov <v.molchanov@dssl.ru>
2024-03-01 18:04:38 +0300

    add button ferefication

---
+++ pages/index_page.py
@@ -0,0 +1,18 @@
+from playwright.sync_api import Page
+import config
+
+
+class IndexPage:
+    _BUTTON_GOOGLE_SEARCH = "//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"
+    _LINK_ENGLISH_LANG = "//a[contains(text(), 'English')]"
+
+    def open_index_page(self, page: Page) -> None:
+        page.goto(config.url.DOMAIN)
+
+    def press_link_english_lang(self, page: Page):
+        page.locator(self._LINK_ENGLISH_LANG).click()
+
+    def get_text_from_google_search_button(self, page: Page) -> None:
+        return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
+
+
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,7 +1,15 @@
 import pytest
+import pages
 
 
 class TestFooter:
 
+
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
-        pass
+        pages.index_page.open_index_page(page)
+        pages.index_page.press_link_english_lang(page)
+        actual_result = pages.index_page.get_text_from_google_search_button(page)
+        assert actual_result == 'Google Search', 'Google Search button is not correct'
+
+
+
