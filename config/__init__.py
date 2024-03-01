999a8c8988488594eeb6597eda545b55a0c4381d
v.molchanov <v.molchanov@dssl.ru>
2024-03-01 15:21:52 +0300

    Настроить окружение

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+from config.url import Url
+from config.playwright import Playwright
+from config.expectations import Expectations
+
+url = Url()
+playwright = Playwright()
+expectations = Expectations()
+
